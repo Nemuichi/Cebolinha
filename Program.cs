f252f028a402da2294140335418d7c961ce6c04f
Ramon Dias <ramondiasperes04@gmail.com>
2025-09-25 19:42:33 -0300

    Programa feito

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string frase, fraseCebolinha;
+
+Console.Write("Escleva uma flase: ");
+
+frase = Console.ReadLine()!;
+
+fraseCebolinha = frase.Replace("r", "l").Replace("R", "L")!;
+
+Console.WriteLine(fraseCebolinha);
+
+
+
+
